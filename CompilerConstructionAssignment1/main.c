a794243c7c216a1aef81d9b89d089774f6c9f77c
Yusuke Iwama <yusuke.iwama@gmail.com>
2013-12-24 12:44:17 +0900

    Initial Commit

---
+++ CompilerConstructionAssignment1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CompilerConstructionAssignment1
+//
+//  Created by Yusuke Iwama on 12/24/13.
+//  Copyright (c) 2013 University of Tsukuba. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
