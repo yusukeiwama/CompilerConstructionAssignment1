68487cb06bf909d7e1a7952430c17d613592eab6
Yusuke IWAMA <yusuke.iwama@gmail.com>
2013-12-31 11:29:37 +0900

    sufficient functionality has been implemented

--- CompilerConstructionAssignment1/readExpr.c
+++ CompilerConstructionAssignment1/readExpr.c
@@ -6,67 +6,92 @@
 //  Copyright (c) 2013 University of Tsukuba. All rights reserved.
 //
 
+/*
+ Top-Down Parser of Four Arithmetic Operations
+ based on the following BNF.
+ 
+ <expression> := <term> <expression1>
+ <expression1> := <expr_op> <term> <expression1> | e
+ <term> := <factor> <term1>
+ <term1> := <term_op> <factor> <term1> | e
+ <factor> := number | '(' <expression> ')'
+ <expr_op> := '+' | '-'
+ <term_op> := '*' | '/'
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "exprParser.h"
 
-AST *readNum(void);
+AST *readTerm(void);
+AST *readFactor(void);
 
-AST *readExpr()
+
+AST *readExpr(void)
 {
-//    int t;
     AST *e, *ee;
 	
-    e = readNum();
+	e = readTerm();
 	
     while (currentToken == PLUS_OP
-		   || currentToken == MINUS_OP
-		   || currentToken == TIMES_OP
-		   || currentToken == OVER_OP) {
+		   || currentToken == MINUS_OP) {
 		
 		ee = (AST *)malloc(sizeof(AST)); // look-ahead
 		ee->op = currentToken;
-		
-		switch (ee->op) {
-			case PLUS_OP:
-			case MINUS_OP:
-				getToken();
-				ee->left = e;
-				ee->right = readNum();
-				e = ee;
-				break;
-			case TIMES_OP:
-			case OVER_OP:
-				getToken();
-				ee->left = e->right;
-				e->right = ee;
-				ee->right = readNum();
-				break;
-			default:
-				break;
-		}
+		getToken();
+		ee->left = e;
+		ee->right = readTerm();
+		e = ee;
     }
     
 	return e;
 }
 
-AST *readNum()
+
+AST *readTerm(void)
 {
-    AST *e;
-    
-	if (currentToken == NUM) { // Top-down parsing
-
-		e = (AST *)malloc(sizeof(AST));
-		e->op = NUM;
-		e->val = tokenVal;
+	AST *e, *ee;
+	
+	e = readFactor();
+	
+	while (currentToken == TIMES_OP
+		   || currentToken == OVER_OP) {
+		
+		ee = (AST *)malloc(sizeof(AST));
+		ee->op = currentToken;
 		getToken();
-		
-		return e;
+		ee->left = e;
+		ee->right = readFactor();
+		e = ee;
+	}
+	
+	return e;
+}
 
-	} else {
-		
-		fprintf(stderr, "bad expression: NUM expected\n"); // Top-down parsing
-		
-		exit(1);
-    }
+
+AST *readFactor(void)
+{
+	AST *e;
+	
+	switch (currentToken) {
+		case NUM:
+			e = (AST *)malloc(sizeof(AST));
+			e->op = NUM;
+			e->val = tokenVal;
+			getToken();
+			return e;
+		case LEFT_PAR:
+			getToken();
+			e = readExpr();
+			if (currentToken == RIGHT_PAR) {
+				getToken();
+				return e;
+			} else {
+				fprintf(stderr, "bad expression: RIGHT_PAR is expected\n"); // Top-down parsing
+				exit(1);
+			}
+		default:
+			fprintf(stderr, "bad expression: NUM or LEFT_PAR is expected\n"); // Top-down parsing
+			exit(1);
+	}
 }
